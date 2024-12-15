6167fae69d8615b8693f724229262399e1ad7c0e
andrew-brown10 <166345061+andrew-brown10@users.noreply.github.com>
2024-12-14 21:40:48 -0500

    Initial Commit

---
+++ GiftedApp/ContentView.swift
@@ -0,0 +1,38 @@
+//
+//  ContentView.swift
+//  GiftedApp
+//
+//  Created by Andrew Brown on 12/14/24.
+//
+
+import SwiftUI
+import SwiftData
+
+struct ContentView: View {
+    @Environment(\.modelContext) private var modelContext
+    @Query private var items: [Item]
+
+    var body: some View {
+        Circle()
+    }
+
+    private func addItem() {
+        withAnimation {
+            let newItem = Item(timestamp: Date())
+            modelContext.insert(newItem)
+        }
+    }
+
+    private func deleteItems(offsets: IndexSet) {
+        withAnimation {
+            for index in offsets {
+                modelContext.delete(items[index])
+            }
+        }
+    }
+}
+
+#Preview {
+    ContentView()
+        .modelContainer(for: Item.self, inMemory: true)
+}
