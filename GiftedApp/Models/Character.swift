149a0c0b9c2342a4f3e082d32e9773f6e4ecee78
andrew-brown10 <166345061+andrew-brown10@users.noreply.github.com>
2024-12-15 18:54:48 -0500

    change item to character

---
+++ GiftedApp/Models/Character.swift
@@ -0,0 +1,16 @@
+//
+//  Item.swift
+//  GiftedApp
+//
+//  Created by Andrew Brown on 12/14/24.
+//
+
+import Foundation
+import SwiftData
+
+struct Character: Codable {
+    let Id: Int
+    let Name: String
+    let Relation: String
+    let Description: String
+}
